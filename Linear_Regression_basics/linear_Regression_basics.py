077994a2f705b974e33eafc42d8bdaf9b1e72cde
Jose R. Prieto <prietofontcuberta@gmail.com>
2017-07-10 15:24:14 +0200

    r-squared

--- Linear_Regression_basics/linear_Regression_basics.py
+++ Linear_Regression_basics/linear_Regression_basics.py
@@ -4,6 +4,15 @@ import matplotlib.pyplot as plt
 from matplotlib import style
 style.use('ggplot')
 
+def squared_error(ys_orig,ys_line):
+    return sum((ys_line - ys_orig) * (ys_line - ys_orig))
+
+def coefficient_of_determination(ys_orig,ys_line):
+    y_mean_line = [mean(ys_orig) for y in ys_orig]
+    squared_error_regr = squared_error(ys_orig, ys_line)
+    squared_error_y_mean = squared_error(ys_orig, y_mean_line)
+    return 1 - (squared_error_regr/squared_error_y_mean)
+
 xs = np.array([1, 2, 3, 4, 5], dtype=np.float64)
 ys = np.array([5, 4, 6, 5, 6], dtype=np.float64)
 
@@ -18,17 +27,22 @@ def best_fit_slope_and_intercept(xs, ys):
 
 # weights
 m, b = best_fit_slope_and_intercept(xs, ys)
-print(m, b)
+print("m %f b %f" %(m, b))
 regression_line = [(m*x)+b for x in xs]
 
-# Predict
-predict_x = 7
-predict_y = (m*predict_x)+b
-print("Predict :")
-print(predict_y)
+r_squared = coefficient_of_determination(ys,regression_line)
+print(r_squared)
 
-plt.scatter(xs,ys,color='#003F72',label='data')
-plt.scatter(predict_x,predict_y,color='#003F00',label='prediction')
-plt.plot(xs, regression_line, label='regression line')
-plt.legend(loc=4)
-plt.show()
+# Predict
+# predict_x = 7
+# predict_y = (m*predict_x)+b
+# print("Predict :")
+# print(predict_y)
+
+
+
+# plt.scatter(xs,ys,color='#003F72',label='data')
+# plt.scatter(predict_x,predict_y,color='#003F00',label='prediction')
+# plt.plot(xs, regression_line, label='regression line')
+# plt.legend(loc=4)
+# plt.show()
